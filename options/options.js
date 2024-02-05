6b4aa990f9203b28c9e64123aaa9b7931a76397b
Crowdbotics <team@crowdbotics.com>
2024-02-05 16:34:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crmapp-45915",
+  url: "https://crmapp-45915.botics.co",
+  api: "https://crmapp-45915.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
